ccbc5f16f4a4cddadcac4be6a64301205263ab2e
unknown <k.osawa>
2026-06-01 15:23:51 +0900

    modified test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,13 +11,21 @@ import {
   provideFirestore } from '@angular/fire/firestore';
 import { provideServiceWorker } from '@angular/service-worker';
 
-const firebaseConfig = {       
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+const firebaseConfig = { 
+        projectId: 'kanban-app-ffa43',
+        appId: '1:148995567752:web:249ddc3ad647d4d6466f3d',
+        storageBucket: 'kanban-app-ffa43.firebasestorage.app',
+        apiKey: 'AIzaSyBxk9WpwWBezXgU-3N6kKPbNYJnjSxwNnU',
+        authDomain: 'kanban-app-ffa43.firebaseapp.com',
+        messagingSenderId: '148995567752',
+        projectNumber: '148995567752',
+        version: '2',
+  // apiKey: "",
+  // authDomain: "",
+  // projectId: "",
+  // storageBucket: "",
+  // messagingSenderId: "",
+  // appId: ""
 };
 
 export const appConfig: ApplicationConfig = {
